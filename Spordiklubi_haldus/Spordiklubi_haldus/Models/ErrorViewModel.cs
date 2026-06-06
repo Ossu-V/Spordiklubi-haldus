bfedf0c47e0ae7b64329f326e32238e3370e6fa0
Ossu-V <oscarvalk7@gmail.com>
2026-06-06 15:58:58 +0300

    Spordiklubi_haldus_Foundation

---
+++ Spordiklubi_haldus/Spordiklubi_haldus/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Spordiklubi_haldus.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
