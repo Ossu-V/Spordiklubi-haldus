41f780e89275bcb16eab5b0d8362371041f2cbb3
Aron3244 <Aadupill05@gmail.com>
2026-06-07 17:35:04 +0300

    Sport class

---
+++ Spordiklubi_haldus/Spordiklubi_haldus/Models/Sport.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Spordiklubi_haldus.Models
+{
+    public class Sport
+    {
+ 
+            public int Id { get; set; }
+
+            [Required]
+            public string Name { get; set; }
+
+            public string Description { get; set; }
+
+            public ICollection<Training> Trainings { get; set; }
+        }
+    }
