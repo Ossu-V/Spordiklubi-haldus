10538f876af38480eda747c48ec76fbc8523c05a
Aron3244 <Aadupill05@gmail.com>
2026-06-07 17:58:25 +0300

    Update SpordiklubiHaldusContext.cs

--- Spordiklubi_haldus/Spordiklubi_haldus/Data/SpordiklubiHaldusContext.cs
+++ Spordiklubi_haldus/Spordiklubi_haldus/Data/SpordiklubiHaldusContext.cs
@@ -1,6 +1,37 @@
-﻿namespace Spordiklubi_haldus.Data
+﻿using Microsoft.EntityFrameworkCore;
+using Spordiklubi_haldus.Models;
+using System.Reflection.Emit;
+
+namespace Spordiklubi_haldus.Data
 {
-    public class SpordiklubiHaldusContext
+    public class SpordiklubiHaldusContext : DbContext
     {
+        public SpordiklubiHaldusContext(
+            DbContextOptions<SpordiklubiHaldusContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Member> Members { get; set; }
+        public DbSet<Membership> Memberships { get; set; }
+        public DbSet<Sport> Sports { get; set; }
+        public DbSet<Training> Trainings { get; set; }
+        public DbSet<Registration> Registrations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Member>().ToTable("Member");
+            modelBuilder.Entity<Membership>().ToTable("Membership");
+            modelBuilder.Entity<Sport>().ToTable("Sport");
+            modelBuilder.Entity<Training>().ToTable("Training");
+            modelBuilder.Entity<Registration>().ToTable("Registration");
+
+            modelBuilder.Entity<Registration>()
+                .HasKey(r => new
+                {
+                    r.MemberId,
+                    r.TrainingId
+                });
+        }
     }
 }
