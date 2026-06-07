04170de0aa95cb571256e7cc17518b3eeefdaf64
Aron3244 <Aadupill05@gmail.com>
2026-06-07 17:41:45 +0300

    Update Member.cs

--- Spordiklubi_haldus/Spordiklubi_haldus/Models/Member.cs
+++ Spordiklubi_haldus/Spordiklubi_haldus/Models/Member.cs
@@ -1,20 +1,20 @@
-﻿using Spordiklubi_haldus.Models;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
-namespace Spordiklubi_haldus.Data
+namespace Spordiklubi_haldus.Models
 {
     public class Member
     {
         public int Id { get; set; }
 
         [Required]
-        public string FirstName { get; set; }
+        public string FirstName { get; set; } = string.Empty;
 
         [Required]
-        public string LastName { get; set; }
+        public string LastName { get; set; } = string.Empty;
 
         public DateTime JoinDate { get; set; }
 
         public ICollection<Membership> Memberships { get; set; }
+            = new List<Membership>();
     }
 }
