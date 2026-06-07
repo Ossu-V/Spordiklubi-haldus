451b38770e5778b1620d02d99a356612ef013d6b
Aron3244 <Aadupill05@gmail.com>
2026-06-07 17:28:48 +0300

    Membership class

---
+++ Spordiklubi_haldus/Spordiklubi_haldus/Models/Membership.cs.cs
@@ -0,0 +1,17 @@
+﻿namespace Spordiklubi_haldus.Models
+{
+    public class Membership
+    {
+        public int Id { get; set; }
+
+        public string MembershipType { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime ExpiryDate { get; set; }
+
+        public int MemberId { get; set; }
+
+        public Member Member { get; set; }
+    }
+}
