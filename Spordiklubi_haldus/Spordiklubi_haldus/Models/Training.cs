3ae9a3e9ec393b87f70fae6aef55ea7b13cfed87
Aron3244 <Aadupill05@gmail.com>
2026-06-07 17:37:40 +0300

    Training class

---
+++ Spordiklubi_haldus/Spordiklubi_haldus/Models/Training.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Spordiklubi_haldus.Models
+{
+    public class Training
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public DateTime TrainingDate { get; set; }
+
+        public int MaximumMembers { get; set; }
+
+        public int CoachId { get; set; }
+        public Coach Coach { get; set; }
+
+        public int SportId { get; set; }
+        public Sport Sport { get; set; }
+
+        public ICollection<Registration> Registrations { get; set; }
+    }
+}
