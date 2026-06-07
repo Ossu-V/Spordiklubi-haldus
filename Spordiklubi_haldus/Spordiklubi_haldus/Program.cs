06fcd86f2f5bf091d8b7d017b86a89a6e2f4234f
Ossu-V <oscarvalk7@gmail.com>
2026-06-07 18:16:03 +0300

    Members controller/Members CREATE, DELETE, DETAILS,EDIT,INDEX cshtml

--- Spordiklubi_haldus/Spordiklubi_haldus/Program.cs
+++ Spordiklubi_haldus/Spordiklubi_haldus/Program.cs
@@ -1,4 +1,8 @@
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("SpordiklubiHaldusContext") ?? throw new InvalidOperationException("Connection string 'SpordiklubiHaldusContext' not found.");
+
+builder.Services.AddDbContext<SpordiklubiHaldusContext>(options => options.UseSqlServer(connectionString));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
