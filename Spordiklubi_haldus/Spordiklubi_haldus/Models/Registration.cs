0387fa0b7f74a45cd9a6a8afd7cf50c6e9e198b3
Aron3244 <Aadupill05@gmail.com>
2026-06-07 17:37:51 +0300

    Registration class

---
+++ Spordiklubi_haldus/Spordiklubi_haldus/Models/Registration.cs
@@ -0,0 +1,15 @@
+﻿using Spordiklubi_haldus.Data;
+
+namespace Spordiklubi_haldus.Models
+{
+    public class Registration
+    {
+        public int MemberId { get; set; }
+        public Member Member { get; set; }
+
+        public int TrainingId { get; set; }
+        public Training Training { get; set; }
+
+        public DateTime RegistrationDate { get; set; }
+    }
+}
