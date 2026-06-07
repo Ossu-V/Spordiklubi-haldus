483d2462e1afe8494578865b0120da50e63d140a
Aaron <aaronvork22@gmail.com>
2026-06-07 19:11:25 +0300

    TrainerModel

---
+++ Spordiklubi_haldus/Spordiklubi_haldus/Models/Trainer.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Spordiklubi_haldus.Models
+{
+    public class Trainer
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Eesnimi")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Perekonnanimi")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Eriala")]
+        public string Specialization { get; set; }
+    }
+}
